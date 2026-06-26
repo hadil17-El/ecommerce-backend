3c20df564f471e8b35ae7edec55d54f94c5d1c03
hadil17-El <eljeddihadil@gmail.com>
2026-06-26 11:27:24 +0200

    Initial commit

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -5,12 +5,35 @@ import auth from "../middleware.js";
 const router = express.Router();
 
 router.get("/:id",auth,async (req,res)=> {
+  try{
+
   const {id} = req.params
   const [order]=await db.query(
     "SELECT * FROM orders WHERE id=? AND user_id=?",
     [id,req.user.id]
   )
-  res.json(order)
+  if(OrderDetails.length === 0){
+    return res.status(404).json({error:"Order not found"})
+  }
+  const [items] = await db.query(
+     `
+     SELECT oi.id,oi.quantity,oi.price,p.name,p.image
+     FROM order_items oi
+     JOIN products p ON
+            p.id = oi.product_id
+            WHERE oi.order_id = ? 
+     `,
+     [id]
+  )
+  res.json({
+    order: orders[0],
+    items
+  })
+} catch (err){
+  res.status(500).json({
+    error:err.message
+  })
+}
 })
 router.post("/", auth, async (req, res) => {
   const user_id = req.user.id;
