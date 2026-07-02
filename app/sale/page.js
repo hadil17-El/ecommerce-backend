77c87252c24d85ffb0c2b831b0dd20da05246666
hadil17-El <148056429+hadil17-El@users.noreply.github.com>
2026-07-02 12:12:34 +0200

    Initial Commit
    Removed comments explaining loading state and async behavior in useEffect.

--- app/sale/page.js
+++ app/sale/page.js
@@ -7,41 +7,11 @@ import "./sale.css"
 export default function Sale(){
     const [products,setProducts]=useState([])
 
-    /**Perché serve lo state loading?
-Serve per sapere:
-
-👉 “i dati sono pronti o no?”
-
-🔥 senza loading:
-pagina vuota
-poi dati appaiono di colpo
-🟢 con loading:
-{loading ? <p>Loading...</p> : <Grid />}
-
-UX migliore perché:
-
-✔ feedback all’utente
-✔ evita “pagina vuota sospetta” */
+ 
     const [loading,setLoading]=useState(true)
 
     useEffect(()=>{
-        /**
-         * perche non puoi solo chiamare getSaleProducts() direttamente qui? Perche getSaleProducts è async e ritorna una promise e react non aspetta auto il risultato dentro useEffect :
-         * Il termine "promessa" indica un impegno a compiere un'azione che potrebbe richiedere del tempo in futuro (come ad esempio recuperare dati da Internet o leggere un file).
-لماذا نستخدمها؟التعامل مع طلبات الإنترنت (API)
-L'idea è semplice:
-Si ha un processo che non si concluderà immediatamente. Invece di interrompere il programma, si fa una "promessa" che il risultato arriverà in un secondo momento.
-Quindi si usa questo pattern:👉 è solo un “wrapper” per usare async/await
-
-React NON vuole una funzione async direttamente in useEffect.
-
-👉 useEffect deve sempre ritornare:
-
-undefined
-oppure cleanup function
-
-Non una Promise.
-*/
+       
         async function load(){
             const data = await getSaleProducts()
             setProducts(data)
