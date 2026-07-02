5650f667d9c5bd825b1226f3591d75768dd1752a
hadil17-El <148056429+hadil17-El@users.noreply.github.com>
2026-07-02 11:10:24 +0200

    Initial Commit
    Removed commented-out code and added loading state for cart items.

--- app/components/Trends.js
+++ app/components/Trends.js
@@ -1,59 +1,3 @@
-//prodotti
-/*"use client"
-import ProductCard from "./ProductCard"
-import Link from "next/link"
-import "./trends.css"
-import { useEffect, useState } from "react"
-import { getLatestProducts } from "../api/api"
-export default function Trends(){
-const [products,setProducts] = useState([])
-useEffect(()=>{
-    async function load() {
-        const data = await getLatestProducts()
-        setProducts(data)
-
-    }
-    load()
-   
-},[])
-/**
- * 👉 stai duplicando la lista dei prodotti
-
-Esempio:
-
-products = [A, B, C]
-
-diventa:
-
-loopProducts = [A, B, C, A, B, C]
-👉 significa:
-
-“ripeti la lista due volte per creare effetto infinito”
- */
-/** cui ho fatto una protazione del codice per evitare il crash : perche lo spread operator ...  serve per aprire un array e nel mio caso [...products,...products]  signfica ripeti la lista 2 volte. il problema cui che se products non è un array e restituisce undefined  oppure error allora ...products crasha perche spread funziona solo con array/iterabili . 
- * cosa ho fatto: ho aggiunto controllo Array.isArray(products) : controlla  se produts è veramente un array? : esempio Array.isArray([1,2,3]) torna true 
-  */
-/* const loopProducts = Array.isArray(products) ? [...products,...products] : []
-return(
-        <div className="trends-section">
-         <div className="trends-header">
-             <h2 className="trends-title">
-            New Arrivals
-                </h2>   
-        </div>
-        <div className="trends-container">
-
-            {loopProducts.map((product,index)=>(
-                <ProductCard key={index} product={product} />
-            ))}
-        </div>
-        <Link href="/products">
-         <button className="trend-btn">Show All</button>
-        </Link>
-        
-        </div>
-    )
-}*/
 "use client"
 
 import ProductCard from "./ProductCard"
@@ -78,7 +22,6 @@ export default function Trends(){
     const [products,setProducts] = useState([])
 const [loading,setLoading]=useState(true)
  const [cartItems,setCartItems] = useState([])
-//cui con [] : signfica che esegui solo una volta quando il componente si monta
  useEffect(()=>{
     async function loadCart(){
         const data = await getCart()
